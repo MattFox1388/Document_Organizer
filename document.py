7ac0e20ec7ea759c582ce3dfaa028257be6970c4
David <david@podts.com>
2019-02-12 17:38:00 -0600

    Documentation

--- document.py
+++ document.py
@@ -10,18 +10,58 @@ class TDocument(type):
 class Document(ABC, TDocument):
 
     def get_hash(self) -> int:
+        """
+        Returns the hash code of this document.
+        :return: Documents hash
+        """
         return self._hash
 
     def get_keywords(self) -> Mapping[str,int]:
+        """
+        Returns a map of all the keywords in this document and their occurrences.
+        :return: Map of keywords to occurrences
+        """
         return self._keywords
 
+    def get_occurrences(self, keyword: str) -> int:
+        """
+        Returns the number of times a given keyword occurs in this document.
+        :param keyword: keyword in question
+        :return: Occurrences of the keyword
+        """
+        keys = self.get_keywords()
+        if keyword in keys:
+            return keys[keyword]
+        return 0
+
+    def occurs(self, keyword: str):
+        """
+        Returns True if the given keyword exists in this document.
+        :param keyword: The keyword in question
+        :return: True if the keyword exists, False otherwise
+        """
+        return self.get_occurrences(keyword) > 0
+
     def get_Parse_Date(self) -> datetime:
+        """
+        Returns the time when this document was parsed last.
+        :return: Last parse time
+        """
         return self._parse_date
 
     def get_file_path(self) -> str:
+        """
+        Returns the path of the file for this document.
+        :return: path of this document
+        """
         return self._file_path
 
     def is_same(self, doc: TDocument) -> bool:
+        """
+        Returns True whether the given document is a duplicate of this one.
+        :param doc: other document to compare
+        :return: True if they have the same hash code, False otherwise
+        """
         return self.get_hash() == doc.get_hash()
 
     def __init__(self, hash: int, keywords: Mapping[str,int],parse_date: datetime, file_path: str):
--- documentbackend.py
+++ documentbackend.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Collection, Mapping
 
-from document import TDocument
+from document import TDocument, Document
 
 
 class TDocumentBackend(type):
@@ -11,8 +11,43 @@ class TDocumentBackend(type):
 class DocumentBackend(ABC, TDocumentBackend):
 
     @abstractmethod
-    def store(self, doc: TDocument) -> bool:
+    def store(self, docs: Collection[TDocument]) -> bool:
+        """
+        Stores multiple documents into this backend that can be retrieved later.
+        :param docs: A collection of documents that will be stored.
+        :return: True if documents were stored successfully, False otherwise.
+        """
         pass
 
-    def get(self, keyword: str) -> List[TDocument]:
+    @abstractmethod
+    def get(self, keyword: str) -> Collection[TDocument]:
+        """
+        Returns any documents that contain the given keyword.
+        :param keyword: The keyword in question
+        :return: Collection of documents
+        """
         pass
+
+    @abstractmethod
+    def get_duplicates(self) -> Mapping[int,Collection[TDocument]]:
+        """
+        Returns all duplicates that are in this backend.
+        The returned map has document hash codes as keys and Collection of all documents with that hash code as values.
+        :return: Map of all duplicates found
+        """
+        pass
+
+    def get_duplicates_of(self, doc: TDocument) -> Collection[TDocument]:
+        """
+        Returns all duplicates of the given document.
+        It is HIGHLY recommended to override this method for each backend since default implementation gathers all
+        duplicates to check.
+        If there are no duplicates for the document the returned Collection will be empty.
+        :param doc: Document to find duplicates of
+        :return: Collection of documents
+        """
+        dupes = self.get_duplicates()
+        hash = doc.get_hash()
+        if hash in dupes:
+            return dupes[hash]
+        return []
