a8072a1edbf4163edca5e94a6fcc803969f9abfa
evan <weathe22@uwm.edu>
2019-02-19 11:37:10 -0600

    WIP Postgres

--- Database/postgresbackend.py
+++ Database/postgresbackend.py
@@ -3,7 +3,8 @@ from Documents.documentbackend import DocumentBackend
 from typing import Collection, Mapping
 from sqlalchemy import create_engine, Column, String, TIMESTAMP, Integer
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
+
 
 class PostgresBackend(DocumentBackend):
 	db = False
@@ -18,20 +19,71 @@ class PostgresBackend(DocumentBackend):
 		self.db = create_engine(database)
 		super(PostgresBackend, self).__init__()
 
+	def close(self):
+		"""
+		Frees all hardware resources that this backend uses.
+		As sessions are made per method, nothing should need to be closed.
+		:return: None
+		"""
+		return
+
 	# TODO: Implement
 	def store(self, docs: Collection[Document]) -> bool:
+		"""
+		Stores multiple documents into this backend that can be retrieved later.
+		:param docs: A collection of documents that will be stored.
+		:return: True if documents were stored successfully, False otherwise.
+		"""
 		pass
 
 	# TODO: Implement
 	def get(self, keyword: str) -> Collection[Document]:
+		"""
+		Returns any documents that contain the given keyword.
+		:param keyword: The keyword in question
+		:return: Collection of documents
+		"""
+		session = sessionmaker(self.db)
+
+		document_query = session.query(DBDocument)\
+			.join(DBKeywordInstance).join(DBKeyword)\
+			.filter(DBKeyword.keyword == keyword)
+
+
+		documents = []
+		for document in document_query:
+
+		return documents
+
+	# TODO: Implement
+	def get_by_path(self, path: str) -> Document:
+		"""
+		Returns the document associated with a specific file path.
+		If there is no Document already stored for that path this will return None.
+		:param path: the path of the document
+		:return: Document for the path
+		"""
 		pass
 
 	# TODO: Implement
 	def get_duplicates(self) -> Mapping[int, Collection[Document]]:
+		"""
+		Returns all duplicates that are in this backend.
+		The returned map has document hash codes as keys and Collection of all documents with that hash code as values.
+		:return: Map of all duplicates found
+		"""
 		pass
 
 	# TODO: Implement
 	def get_duplicates_of(self, doc: Document) -> Collection[Document]:
+		"""
+		Returns all duplicates of the given document.
+		It is HIGHLY recommended to override this method for each backend since default implementation gathers all
+		duplicates to check.
+		If there are no duplicates for the document the returned Collection will be empty.
+		:param doc: Document to find duplicates of
+		:return: Collection of documents
+		"""
 		pass
 
 
@@ -63,5 +115,7 @@ class DBKeywordInstance(base):
 	_tablename = 'keyword_instance'
 
 	keyword_id = Column(Integer, primary_key=True)
+	keyword = relationship(DBKeyword, backref='keyword_id')
 	file_id = Column(Integer, primary_key=True)
+	file = relationship(DBDocument, backref='file_id')
 
