1dff9d3ca1e4ab3ef359cf5305badf8ff83add9c
David <david@podts.com>
2019-02-20 10:55:04 -0600

    Add get_compatible_extensions method for DocumentParser

--- Documents/documentparser.py
+++ Documents/documentparser.py
@@ -1,4 +1,6 @@
+import os
 from abc import ABC, abstractmethod
+from collections import Collection
 
 from Documents.document import Document
 
@@ -11,13 +13,20 @@ class DocumentParser(ABC):
     __metaclass__ = TDocumentParser
 
     @abstractmethod
+    def get_compatible_extensions(self) -> Collection[str]:
+        """
+        Returns a collection of all file extensions that this parser can parse into documents.
+        :return: collection of file extensions
+        """
+        pass
+
     def can_parse(self, file_path: str) -> bool:
         """
         Returns True if this parser can parse the given file.
         :param file_path: the path of the file
         :return: True if this parser can parse the file
         """
-        pass
+        return os.path.splitext(file_path) in self.get_compatible_extensions()
 
     @abstractmethod
     def parse(self, file_path: str) -> Document:
@@ -27,3 +36,7 @@ class DocumentParser(ABC):
         :return: A document instance representing the parsed file
         """
         pass
+
+    def compute_hash(self, file_path: str) -> int:
+        f = open(file_path, 'r')
+        hash = 31
