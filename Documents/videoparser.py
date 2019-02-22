acde9aa17af441e05206716d9b8af6f4adf92815
MattFox1388 <fox5@uwm.edu>
2019-02-22 00:38:14 -0600

    most of videoparser complete

--- Documents/videoparser.py
+++ Documents/videoparser.py
@@ -1,10 +1,55 @@
 from Documents.documentparser import DocumentParser
-from Documents.document import Document
-import subprocess as subpros
+from .document import Document, SimpleDocument
+import os
+import utc
+import speech_recognition as sr
+from collections import Collection
+import moviepy.editor as mp
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+VIDEO_EXTS = ['.flv', '.avi', '.wmv', '.mov', '.mp4']
+
 
 class VideoParser(DocumentParser):
+
     def can_parse(self, file_path: str) -> bool:
-        pass
+        ext = os.path.splitext(file_path)[1]
+        # only supporting commonly known video extensions
+        # I'll add if needed based on initial testing
+        if ext in VIDEO_EXTS:
+            return True
+        return False
 
     def parse(self, file_path: str) -> Document:
-        pass
+        # hash value
+        video_hash = int(self.compute_hash(file_path, 65536), 16)
+
+        # converting video to audio
+        temp_audio_file = 'video_audio/audio.wav'
+        audio_clip = mp.VideoFileClip(file_path).subclip(0)
+        audio_clip.audio.write_audiofile(temp_audio_file, nbytes=2)
+
+        # transcribing audio using speech recognition
+        r = sr.Recognizer()
+        with sr.AudioFile(temp_audio_file) as source:
+            audio = r.record(source)
+            text = r.recognize_sphinx(audio)
+
+        # filter stopwords
+        stop_words = set(stopwords.words('english'))
+        text_tokens = word_tokenize(text)
+        text_wo_tokens = [word for word in text_tokens if not word in stop_words]
+
+        # only keep words greater than 2
+        keyword_dict = dict()
+        for item in text_wo_tokens:
+            if len(item) > 2:
+                pass
+
+        # TODO: fill keyword_dict
+
+        return SimpleDocument(hash_val=video_hash, keywords=keyword_dict, file_path=file_path, parse_date=utc.now())
+
+    def get_compatible_extensions(self) -> Collection[str]:
+        return VIDEO_EXTS
