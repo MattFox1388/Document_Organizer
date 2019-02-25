91bfd14f7db354b476235f1687df1a67248715ed
Nathan <mikelonisnate@gmail.com>
2019-02-25 17:42:20 -0600

    based file for testing fileparser, and changed up fileparser with nltk

--- Documents/fileparser.py
+++ Documents/fileparser.py
@@ -5,8 +5,11 @@ from Documents.document import SimpleDocument, Document
 import textract
 import os.path
 import re
+import utc
 from string import punctuation
 from collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 EXTENSIONS = {'.csv', '.doc', '.docx', '.eml', '.epub', '.gif', '.jpg', '.jpeg', '.json', '.html', '.htm', '.mp3',
               '.msg', '.odt', '.ogg', '.pdf', '.png', '.pptx', '.ps', '.rtf', '.tiff', '.tif', '.txt', '.wav', 'xlsx',
@@ -17,16 +20,33 @@ splitter = re.compile(r'[\s{}]+'.format(re.escape(punctuation)))
 class FileParser(DocumentParser):
 
     def get_compatible_extensions(self) -> Collection[str]:
-        pass
+        return EXTENSIONS
 
+    """
+    Removes text from in front of the extension and then checks
+    to see if the extension is compatible. If not compatible, it
+    is sent to where it can be parsed. Otherwise, the file
+    is processed into text using textract. The text is then split
+    into a list such that each word is separated and no punctuation
+    exists.
+    Stop words are removed from the list and then it is put into a dictionary
+    """
     def parse(self, file_path: str) -> Document:
         ext = os.path.splitext(file_path)[1]
 
-        if ext in EXTENSIONS:
+        if ext not in EXTENSIONS:
+            # Need to decide how other files will be parsed
+            # if the extension does not exist in EXTENSIONS
+            #
+            # Also need to find out what other files exist
+            # Need to parse through the collection of data
+            # and pull each extension to determine this
+            pass
+        else:
             text = textract.process(file_path)
             word_list = splitter.split(text)[:-1]
-            word_map = dict(Counter(word_list))
-            return SimpleDocument(hash(file_path), word_map, file_path)
-        # Need to decide how other files will be parsed
-        # if the extension does not exist in EXTENSIONS
-        pass
+            stop = set(stopwords.words('english'))
+            tokens = [w for w in word_list if not w in stop]
+            word_map = dict(Counter(tokens))
+            file_hash = int(self.compute_hash(file_path, 65536), 16)
+            return SimpleDocument(hash_val=file_hash, keywords=word_map, file_path=file_path, parse_date=utc.now())
---
+++ Tests/testfileparser.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class TestFileParser(unittest.TestCase):
+    pass
