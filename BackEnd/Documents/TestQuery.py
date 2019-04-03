0c63d4f0e410b3f4ef6314e0609b8c5425e018b8
David <david@podts.com>
2019-04-03 11:44:15 -0500

    get docs implementation

--- BackEnd/Documents/TestQuery.py
+++ BackEnd/Documents/TestQuery.py
@@ -1,7 +1,11 @@
 from sabackend import SABackend
+import sys
 
 back = SABackend('ceas-e384d-dev1.cs.uwm.edu', 'documentorganizer', 'doc_org', 'd3NXWWfyHT', '5432')
 
-docs = back._get_docs('java')
+
+query = sys.argv[0]
+
+docs = back._get_docs(query)
 for doc in docs:
     print(doc.get_file_path())
