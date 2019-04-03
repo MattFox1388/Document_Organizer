8e0424cd7160dce667ab31054ceeadffe9ab79a6
David <david@podts.com>
2019-04-03 11:42:12 -0500

    get docs implementation

--- BackEnd/Documents/TestQuery.py
+++ BackEnd/Documents/TestQuery.py
@@ -2,4 +2,6 @@ from sabackend import SABackend
 
 back = SABackend('ceas-e384d-dev1.cs.uwm.edu', 'documentorganizer', 'doc_org', 'd3NXWWfyHT', '5432')
 
-print(back._get_docs('mercury'))
+docs = back._get_docs('java')
+for doc in docs:
+    print(doc.get_file_path())
