e95ea68a34ceed00a9f63356e1fef62fd2f1e170
David <david@podts.com>
2019-04-14 13:24:12 -0500

    Alter document retrieval

--- BackEnd/Documents/sabackend.py
+++ BackEnd/Documents/sabackend.py
@@ -210,7 +210,7 @@ class SABackend(StorageBackend):
         FROM keyword_instance \
         LEFT JOIN keyword on keyword.keyword_id = keyword_instance.keyword_id \
         WHERE keyword.keyword LIKE '" + keyword + "' \
-        ORDERBY tf_idf('" + keyword + "', file_id);")
+        ORDER BY tf_idf('" + keyword + "', file_id);")
         ids = [row[0] for row in result]
         return self.session().query(SADocument).filter(SADocument.file_id.in_(ids)).all()
 
